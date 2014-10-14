11d05c29f3c9e263d2150aec17ebac4350c07cca
Gareth Ashcroft <garethashcroft@gmail.com>
2014-10-14 16:04:37 +0100

    pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -9,14 +10,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -42,6 +43,10 @@ class PinsController < ApplicationController
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
+    def correct_user
+      @pin = current_user.pins.find_by(params[:id])
+      redirect_to pins_path if @pin.nil?
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
